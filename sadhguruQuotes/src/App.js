4d3fb4a58b1d436216d62b802f925b5163ad24e1
Sudarshan K J <kjsudi@gmail.com>
2021-03-03 12:06:39 -0800

    Added GMT to date, remvoed unnecesary computation of adding offset to fetch new trigger date

--- sadhguruQuotes/src/App.js
+++ sadhguruQuotes/src/App.js
@@ -51,11 +51,9 @@ function App() {
     //check if quote is outdated and if there exists new quote for today
     const storedQuoteObj = storedQuote();
     const today = new Date();
-    const offset = today.getTimezoneOffset() / 60;
-    today.setHours(today.getHours() + offset);
     if (storedQuoteObj) {
       const nextTriggerDate = new Date(storedQuote().publishedDate);
-      nextTriggerDate.setHours(nextTriggerDate.getHours() + offset + 24); // 24 is added so that 1 day post the previous published date, we start triggering the auto add api
+      nextTriggerDate.setHours(nextTriggerDate.getHours() + 24); // 24 is added so that 1 day post the previous published date, we start triggering the auto add api
       //Tweets are posted exactly at 2:45 GMT. 2nd March 2:45 GMT tweet posted. Now it is, 2nd March 2:00 GMT ( or 6PM PST ). Current recorded tweet is 1st March 2:45 GMT.
       if (nextTriggerDate) {
         if (today.valueOf() > nextTriggerDate.valueOf()) {
@@ -110,7 +108,7 @@ function App() {
       <div className="app">
         <QuoteCard
           key={quote.quote}
-          publishedDate={getPublishdedDate()}
+          publishedDate={`${getPublishdedDate()} GMT`}
           quoteImage={quote.imageLink}
         >
           {quote.isLoading || !quote.quote ? "Loading..." : quote.quote}
