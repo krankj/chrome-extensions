3148465218158d516541b8917e046a03b5010863
Sudarshan K J <kjsudi@gmail.com>
2021-03-16 16:53:00 +0530

    Final content layout modification done. This will be the final draft

--- sadhguruQuotes/src/components/SideDrawer.js
+++ sadhguruQuotes/src/components/SideDrawer.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./SideDrawer.css";
 import classNames from "classnames";
 import SocialIcons from "./SocialIcons";
+import { ReactComponent as ClockIcon } from "../assets/icons/wall-clock.svg";
+import { ReactComponent as ContentIcon } from "../assets/icons/content.svg";
+import { ReactComponent as MadeIcon } from "../assets/icons/tools.svg";
 
 const SideDrawer = ({ isOpen, handleDrawer }) => {
   return (
@@ -13,18 +16,38 @@ const SideDrawer = ({ isOpen, handleDrawer }) => {
         &#9776;
       </span>
       <div className={classNames("sidenav", { open: isOpen })}>
-        <button className="closebtn" onClick={handleDrawer}>
-          &times;
-        </button>
         <div
           key={isOpen}
-          className={classNames("contactContainer", { hide: !isOpen })}
+          className={classNames("contentContainer", { hide: !isOpen })}
         >
-          <h1>Made with love and grace by</h1>
-          <p>Sudarshan K J</p>
-          <p>kjsudi@gmail.com</p>
-          <p>+919686678568</p>
-          <SocialIcons />
+          <button className="closebtn" onClick={handleDrawer}>
+            &times;
+          </button>
+          <div className="infoContainer">
+            <ClockIcon className="icon" />
+            <p>Quotes are updated everyday at 8:15 AM IST</p>
+          </div>
+          <div className="infoContainer">
+            <ContentIcon className="icon" />
+            <p>
+              All the content is proprietary of{" "}
+              <a
+                href="https://isha.sadhguru.org/in/en/wisdom/type/quotes"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Isha Foundation
+              </a>
+            </p>
+          </div>
+          <div className="infoContainer">
+            <MadeIcon className="icon" />
+            <h1>Made with love and grace by</h1>
+            <p>Sudarshan K J</p>
+            <p>kjsudi@gmail.com</p>
+            <p>+919686678568</p>
+            <SocialIcons />
+          </div>
         </div>
       </div>
     </div>
