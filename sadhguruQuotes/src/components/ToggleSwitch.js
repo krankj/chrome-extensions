6628e2eb8cab5e4feda83299d2893a2345fddf8b
Sudarshan K J <kjsudi@gmail.com>
2021-03-26 17:37:16 +0530

    Added tooltip

--- sadhguruQuotes/src/components/ToggleSwitch.js
+++ sadhguruQuotes/src/components/ToggleSwitch.js
@@ -1,12 +1,22 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 import "./ToggleSwitch.css";
 
 const ToggleSwitch = (props) => (
   <>
-    <label className="switch">
-      <input type="checkbox" />
+    <label
+      data-tip="Turn this on to get random quotes on every new tab"
+      className="switch"
+    >
+      <input data-tip="hello world" type="checkbox" />
       <span className="slider round"></span>
     </label>
+    <ReactTooltip
+      place="bottom"
+      delayShow={100}
+      textColor="#f9f5ee"
+      backgroundColor="rgb(138, 70, 6)"
+    />
   </>
 );
 
