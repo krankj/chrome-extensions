ec32c6a9fe4061976adc5e255f45fd231ae73168
Sudarshan K J <kjsudi@gmail.com>
2021-05-06 21:29:19 +0530

    Implemented a working file cache using file locks

--- dailyMysticQuotes_ChromeExt_SG/aws-lambdas/aws-api-lambda-fetch-quotes-sg-ce/handler.js
+++ dailyMysticQuotes_ChromeExt_SG/aws-lambdas/aws-api-lambda-fetch-quotes-sg-ce/handler.js
@@ -6,15 +6,17 @@ logger.level = "debug";
 const { readData } = require("./dynamo");
 const app = express();
 const fs = require("fs").promises;
+const os = require("os");
+const path = require("path");
+const lockfile = require("proper-lockfile");
 
 app.use(cors());
 app.use(express.json());
 app.disable("x-powered-by");
 
-const LOCAL_CACHE_FILE_NAME = "quotesLocalFile.json";
-const ADD_MINS_FOR_NEXT_TRIGGER = 1455;
+const LOCAL_CACHE_FILE_NAME = `${os.tmpdir()}${path.sep}quotesLocalFile.json`;
 
-const fetchFromDbAndCacheLocally = async () => {
+const fetchFromDb = async () => {
   try {
     logger.info("< Fetching latest quote from DB >");
     console.time("[DB_PULL]");
@@ -22,6 +24,19 @@ const fetchFromDbAndCacheLocally = async () => {
     logger.info("< Successfully fetched latest quote from DB >");
     console.timeEnd("[DB_PULL]");
     const data = response.Items[0];
+    return data;
+  } catch (e) {
+    logger.error(
+      "< Someting went wrong while running function 'fetchFromDb'. Error -> ",
+      e
+    );
+  }
+};
+
+const fetchFromDbAndCacheLocally = async () => {
+  try {
+    const data = await fetchFromDb();
+    data.isFile = true;
     logger.info("< Attempting to insert new data to file >");
     console.time("[FILE_WRITE]");
     await fs.writeFile(LOCAL_CACHE_FILE_NAME, JSON.stringify(data), "utf-8");
@@ -33,6 +48,7 @@ const fetchFromDbAndCacheLocally = async () => {
       "< Someting went wrong while running function 'fetchFromDbAndCacheLocally'. Error -> ",
       e
     );
+    process.exit(1);
   }
 };
 
@@ -44,38 +60,44 @@ app.get("/quotes", async (req, res, next) => {
       if (!dataFromFile) throw new Error("< Empty file >");
       dataFromFile = JSON.parse(dataFromFile);
     } catch (e) {
+      console.log("Error is", e);
       logger.warn("< !!! File contains empty / invalid data !!! >");
-      const data = await fetchFromDbAndCacheLocally();
-      logger.info("< OK: Returning data from DB >");
-      return res.status(200).send({ data });
+      const data = await fetchFromDb();
+      if (data) return res.status(200).send({ data });
+      else return res.status(500).send({ message: "Something went wrong" });
     }
-    let now = new Date();
-    let nextTriggerDate = new Date(dataFromFile.publishedDate);
-    nextTriggerDate.setMinutes(
-      nextTriggerDate.getMinutes() + ADD_MINS_FOR_NEXT_TRIGGER
-    );
-    if (now.valueOf() <= nextTriggerDate.valueOf()) {
-      logger.info("< OK: Returning data from Local File Cache >");
-      return res.status(200).send({ data: dataFromFile });
-    } else {
-      const data = await fetchFromDbAndCacheLocally();
-      if (data) {
-        logger.info("< OK: Returning NEW data from DB >");
-        return res.status(200).send({ data });
-      }
-      return res.status(500).send({
-        error: "Something went wrong while fetching new quote from db",
-      });
-    }
+    logger.info("< OK: Returning data from Local File Cache >");
+    return res.status(200).send({ data: dataFromFile });
   } catch (e) {
     logger.error(
       "Something went wrong while reading data from file. Error: ",
       e
     );
     logger.warn("< Responding with data directly from db >");
-    const response = await readData(11111);
-    return res.status(200).send({ data: response.Items[0] });
+    const data = await fetchFromDb();
+    return res.status(200).send({ data });
   }
+});
+
+app.post("/cacheQuoteLocally", async (req, res) => {
+  await fs.writeFile(LOCAL_CACHE_FILE_NAME);
+  lockfile
+    .lock(LOCAL_CACHE_FILE_NAME)
+    .then(async (release) => {
+      try {
+        await fetchFromDbAndCacheLocally();
+        release();
+      } catch (e) {
+        console.log("Error is", e);
+        logger.error("Something wrong occurred while caching file locally");
+      }
+    })
+    .catch((e) => {
+      console.error("Error occurred ->", e);
+    });
+  return res
+    .status(200)
+    .send({ ok: "Initiated request to cache data locally" });
 });
 
 app.use((req, res, next) => {
