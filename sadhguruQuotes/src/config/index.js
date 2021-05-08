232c288e457761bd601be92d65fccb53c977f3b7
Sudarshan K J <kjsudi@gmail.com>
2021-05-08 11:38:44 +0530

    Updated trigger time

--- sadhguruQuotes/src/config/index.js
+++ sadhguruQuotes/src/config/index.js
@@ -6,12 +6,12 @@ const config =
         API_ENDPOINT: `http://${ip.address()}:9000`,
         SG_PRIVATE_KEY: "sadhguru",
         //Tweets are posted exactly at 2:45 GMT everyday, so start trigggering an api call only after 2:45GMT the next day
-        ADD_MINS_TO_TRIGGER: 1440,
+        ADD_MINS_TO_TRIGGER: 1455,
       }
     : {
         API_ENDPOINT: "https://sadhguru-backend.vercel.app",
         SG_PRIVATE_KEY: "$@dhGuRu",
-        ADD_MINS_TO_TRIGGER: 1440,
+        ADD_MINS_TO_TRIGGER: 1455,
       };
 
 export default config;
